6748243d71076abb848c88a006dfce6c4d979b94
Khoerunisa Salma <115153134+NisaSalma@users.noreply.github.com>
2022-11-18 14:07:21 +0700

    Add files via upload

---
+++ soal 1.py
@@ -0,0 +1,5 @@
+1  "Integer digunakan untuk mendeklarasikan data/variable sebagai bilangan bulat. Misalkan nilai 10,7,29,100, dan  22"
+   "Float tipe data ini digunakan untuk data angka, namun untuk angka/nilai yang berupa pecahan. Contohnya nilai 1.5, 3.5, 3.14"
+   "Char digunakan untuk deklarasi variable berupa karakter. Karakter tersebut bisa berupa simbol, alfabet, atau angka, contohnya" 'ayo belajar bahasa python'.
+   "String tipe data yang digunakan untuk mendeklarasikan data yang berisi kalimat atau kata. contohnya 'Ayo belajar Python'"
+
---
+++ soal 2.py
@@ -0,0 +1,10 @@
+2. #tipe data Float
+    print(3.14)
+    #tipe data Complex
+    print(5j)
+    #tipe data List
+    print([1,2,3,4,5])
+    print(["satu", "dua", "tiga"])
+    #tipe data Tuple
+    print((1,2,3,4,5))
+    print(("satu", "dua", "tiga"))
